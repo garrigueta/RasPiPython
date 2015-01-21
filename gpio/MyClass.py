16d733c71d53279e9aaddb48b7bd14f1860191cb
Garrigueta <ferran.garriga@gmail.com>
2015-01-21 14:15:01 +0100

    first commit

---
+++ gpio/MyClass.py
@@ -0,0 +1,8 @@
+__author__ = "Garrigueta7msi"
+__date__ = "$21-ene-2015 11:11:47$"
+
+class MyClass:
+    """A simple example class"""
+    i = 123456
+    def f(self):
+        return 'hello world'
---
+++ gpio/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "Garrigueta7msi"
+__date__ = "$21-ene-2015 11:21:25$"
